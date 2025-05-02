c554ac689e4804c49a3822c18e8a0eb66ffc2e58
josh-1865 <144809020+josh-1865@users.noreply.github.com>
2025-05-02 10:55:21 +0100

    Update mobile-navigation.blade.php

--- resources/views/components/ui/mobile-navigation.blade.php
+++ resources/views/components/ui/mobile-navigation.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="flex justify-end">
     <button data-collapse-toggle="mobile-menu-2" type="button"
-        class="ml-2 inline-flex items-center rounded-lg p-2 text-sm hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-secondary-200 dark:hover:bg-secondary-950 dark:focus:ring-secondary-800 lg:hidden"
+        class="inline-flex items-center rounded-lg p-2 text-sm hover:bg-secondary-100 focus:outline-none focus:ring-2 focus:ring-secondary-200 dark:hover:bg-secondary-950 dark:focus:ring-secondary-800 lg:hidden"
         aria-controls="mobile-menu-2" aria-expanded="false">
         <span class="sr-only">{{ __('Open Main Menu') }}</span>
         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
